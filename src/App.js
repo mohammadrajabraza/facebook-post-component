7c7d2ccb85e79b6c3aa04c4ba220a6c7162b559d
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-02 10:41:34 +0500

    data added to be passed to component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const data = {
+    createdBy: 'Rajab Raza',
+    avatar: 'https://picsum.photos/id/338/90',
+    description: 'Lots of problems begin with citing wikipedia!',
+    images : [
+              'https://picsum.photos/id/312/450/600', 
+              'https://picsum.photos/id/319/780/650',
+              'https://picsum.photos/id/322/635/450',
+              'https://picsum.photos/id/328/1050/800',
+              'https://picsum.photos/id/336/850/400'
+            ],
+    createdAt: Date.now()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
